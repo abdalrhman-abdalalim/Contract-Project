98d58eca867436c4697460636e5367bf36a35c07
abdalrhman-abdalalim <abdalrhmanaboalela162@gmail.com>
2024-12-10 13:37:19 +0200

    update photo importing method

--- src/data/indext.ts
+++ src/data/indext.ts
@@ -1,57 +1,68 @@
+// Import all images
+import photo1 from "../../assets/Gallery/photo_5961075184628778086_y.png";
+import photo2 from "../../assets/Gallery/photo_5961075184628778087_x.png";
+import photo3 from "../../assets/Gallery/photo_5961075184628778091_x.png";
+import photo4 from "../../assets/Gallery/photo_5961075184628778105_y.png";
+import photo5 from "../../assets/Gallery/photo_5961075184628778107_y.png";
+import photo6 from "../../assets/Gallery/photo_5961075184628778110_x.png";
+import photo7 from "../../assets/Gallery/photo_5961075184628778111_y.png";
+import photo8 from "../../assets/Gallery/photo_5961075184628778113_x.png";
+import photo9 from "../../assets/Gallery/photo_5961075184628778114_y.png";
+
 import { IImage } from "../interface/indext";
 
 export const PhotoService: IImage[] = [
   {
-    itemImageSrc: "assets\\Gallery\\photo_5961075184628778086_y.png",
-    thumbnailImageSrc: "assets\\Gallery\\photo_5961075184628778086_y.png",
+    itemImageSrc: photo1,
+    thumbnailImageSrc: photo1,
     alt: "Description for Image 1",
     title: "Title 1",
   },
   {
-    itemImageSrc: "assets\\Gallery\\photo_5961075184628778087_x.png",
-    thumbnailImageSrc: "assets\\Gallery\\photo_5961075184628778087_x.png",
+    itemImageSrc: photo2,
+    thumbnailImageSrc: photo2,
     alt: "Description for Image 2",
     title: "Title 2",
   },
   {
-    itemImageSrc: "assets\\Gallery\\photo_5961075184628778091_x.png",
-    thumbnailImageSrc: "assets\\Gallery\\photo_5961075184628778091_x.png",
+    itemImageSrc: photo3,
+    thumbnailImageSrc: photo3,
     alt: "Description for Image 3",
     title: "Title 3",
   },
   {
-    itemImageSrc: "assets\\Gallery\\photo_5961075184628778105_y.png",
-    thumbnailImageSrc: "assets\\Gallery\\photo_5961075184628778105_y.png",
+    itemImageSrc: photo4,
+    thumbnailImageSrc: photo4,
     alt: "Description for Image 4",
     title: "Title 4",
   },
   {
-    itemImageSrc: "assets\\Gallery\\photo_5961075184628778107_y.png",
-    thumbnailImageSrc: "assets\\Gallery\\photo_5961075184628778107_y.png",
+    itemImageSrc: photo5,
+    thumbnailImageSrc: photo5,
     alt: "Description for Image 5",
     title: "Title 5",
   },
   {
-    itemImageSrc: "assets\\Gallery\\photo_5961075184628778110_x.png",
-    thumbnailImageSrc: "assets\\Gallery\\photo_5961075184628778110_x.png",
+    itemImageSrc: photo6,
+    thumbnailImageSrc: photo6,
     alt: "Description for Image 6",
     title: "Title 6",
   },
   {
-    itemImageSrc: "assets\\Gallery\\photo_5961075184628778111_y.png",
-    thumbnailImageSrc: "assets\\Gallery\\photo_5961075184628778111_y.png",
+    itemImageSrc: photo7,
+    thumbnailImageSrc: photo7,
     alt: "Description for Image 7",
     title: "Title 7",
   },
   {
-    itemImageSrc: "assets\\Gallery\\photo_5961075184628778113_x.png",
-    thumbnailImageSrc: "assets\\Gallery\\photo_5961075184628778113_x.png",
+    itemImageSrc: photo8,
+    thumbnailImageSrc: photo8,
     alt: "Description for Image 8",
     title: "Title 8",
   },
   {
-    itemImageSrc: "assets\\Gallery\\photo_5961075184628778114_y.png",
-    thumbnailImageSrc: "assets\\Gallery\\photo_5961075184628778114_y.png",
+    itemImageSrc: photo9,
+    thumbnailImageSrc: photo9,
     alt: "Description for Image 9",
     title: "Title 9",
   },
